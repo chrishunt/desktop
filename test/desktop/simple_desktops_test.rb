2b0b3ab721ed8771fdf78d77934868b201cbd806
Chris Hunt <chrishunt@github.com>
2014-05-27 17:35:15 -0700

    Record SimpleDesktops#latest test with VCR

--- test/desktop/simple_desktops_test.rb
+++ test/desktop/simple_desktops_test.rb
@@ -5,9 +5,11 @@ module Desktop
   describe SimpleDesktops do
     describe '#latest' do
       it 'returns the url for the latest desktop image' do
-        assert_equal \
-          'http://static.simpledesktops.com/uploads/desktops/2014/05/16/Pixelmoon_by_ArMaNDJ.png',
-          SimpleDesktops.new.latest
+        VCR.use_cassette('simple_desktops_latest') do
+          assert_equal \
+            'http://static.simpledesktops.com/Pixelmoon_by_ArMaNDJ.png',
+            SimpleDesktops.new.latest
+        end
       end
     end
   end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,12 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require 'pry'
+require 'vcr'
 
 # add lib to load path
 $: << 'lib'
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'test/fixtures'
+  c.hook_into :webmock
+end
