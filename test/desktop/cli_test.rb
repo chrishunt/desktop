35d0732af45bf274006d8d9b0723dc524aa2c0f2
Chris Hunt <chrishunt@github.com>
2014-06-01 10:45:02 -0700

    Change CLI test helper to with_defaults

--- test/desktop/cli_test.rb
+++ test/desktop/cli_test.rb
@@ -3,42 +3,37 @@ require 'desktop/cli'
 
 module Desktop
   describe CLI do
-    def cli(*args)
+    def with_defaults
       desktop = Tempfile.new(%w[desktop .jpg])
       desktop.write 'Default content'
       desktop.rewind
 
-      CLI.start Array(args) + [
-        "--default-image-path=#{desktop.path}",
-        "--skip-reload"
-      ]
-
-      yield desktop
-    ensure
-      desktop.unlink
-    end
-
-    def with_new_image
       image = Tempfile.new(%w[image .jpg])
       image.write 'New content'
       image.rewind
 
-      yield image
+      defaults = [
+        "--default-image-path=#{desktop.path}",
+        "--skip-reload"
+      ]
+
+      yield desktop, image, defaults
     ensure
+      desktop.unlink
       image.unlink
     end
 
     it 'sets the desktop when a local image path is provided' do
-      with_new_image do |image|
-        cli 'set', image.path do |desktop|
-          assert_equal File.read(desktop), File.read(image)
-        end
+      with_defaults do |desktop, image, defaults|
+        CLI.start ['set', image.path] + defaults
+        assert_equal File.read(desktop), File.read(image)
       end
     end
 
     it 'sets the desktop when a web url is provided' do
-      VCR.use_cassette('web_image_data') do
-        cli 'set', 'http://f.cl.ly/image.jpg' do |desktop|
+      with_defaults do |desktop, _, defaults|
+        VCR.use_cassette('web_image_data') do
+          CLI.start ['set', 'http://f.cl.ly/image.jpg'] + defaults
           refute_equal 'Default content', File.read(desktop)
         end
       end
