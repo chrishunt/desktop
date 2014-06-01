088c61fea5dbf03d0838f4b1e8dc8e35a1f99232
Chris Hunt <chrishunt@github.com>
2014-05-31 21:33:54 -0700

    Update doc string for CLI#set

--- lib/desktop/cli.rb
+++ lib/desktop/cli.rb
@@ -3,7 +3,7 @@ require 'desktop'
 
 module Desktop
   class CLI < Thor
-    desc 'set PATH', 'set desktop to image at PATH'
+    desc 'set IMAGE_PATH', 'Set all desktops to the image at IMAGE_PATH'
     option :default_image_path
     def set(path)
       osx = OSX.new(options[:default_image_path])
