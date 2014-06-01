25c87209dce1f0341c6a4bed351eaaa067b2a6e6
Chris Hunt <chrishunt@github.com>
2014-05-31 21:42:39 -0700

    Add long description for CLI#set

--- lib/desktop/cli.rb
+++ lib/desktop/cli.rb
@@ -4,7 +4,13 @@ require 'desktop'
 module Desktop
   class CLI < Thor
     desc 'set IMAGE_PATH', 'Set all desktops to the image at IMAGE_PATH'
-    option :default_image_path
+    long_desc <<-LONGDESC
+      `desktop set` will set the desktop image of all spaces on all monitors to
+      the image at `IMAGE_PATH`.
+
+      > $ desktop set /path/to/image.png
+    LONGDESC
+    option :default_image_path, :hide => true
     def set(path)
       osx = OSX.new(options[:default_image_path])
       osx.desktop_image = LocalImage.new(path)
