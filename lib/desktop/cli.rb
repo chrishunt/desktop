866961935e82e4f5a6b8060f29634b1dbbf79e86
Chris Hunt <chrishunt@github.com>
2014-05-31 23:04:33 -0700

    Update permissions in CLI if set fails

--- lib/desktop/cli.rb
+++ lib/desktop/cli.rb
@@ -11,9 +11,26 @@ module Desktop
       > $ desktop set /path/to/image.png
     LONGDESC
     option :default_image_path, :hide => true
-    def set(path)
-      osx = OSX.new(options[:default_image_path])
-      osx.desktop_image = LocalImage.new(path)
+    option :skip_reload, :type => :boolean, :hide => true
+    def set(path, already_failed = false)
+      begin
+        osx = OSX.new(options[:default_image_path], options[:skip_reload])
+        osx.desktop_image = LocalImage.new(path)
+      rescue OSX::DesktopImagePermissionsError => e
+        fail(e) if already_failed
+        print "It looks like this is the first time you've tried to change "
+        puts  "your desktop."
+        puts
+        print "We need to make your desktop image writable before we can "
+        puts  "change it. This only needs to be done once."
+        puts
+        puts "$ #{OSX.chown_command}"
+        puts "$ #{OSX.chmod_command}"
+        puts
+        osx.update_desktop_image_permissions
+
+        set path, true
+      end
     end
   end
 end
